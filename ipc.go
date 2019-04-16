57b90f59a349510fe308099564fc630cef404a18
goblinbear <goblin5417@gmail.com>
2019-04-16 10:58:20 +0800

    Add new, write,read.

---
+++ ipc.go
@@ -0,0 +1,56 @@
+package hexipc
+
+import (
+	"fmt"
+	"net"
+    
+    "beson"
+    "beson/types"
+)
+
+const socketFile string = "/home/bear/hex.sock"
+var connection net.Conn = nil
+
+func NewHexIpc() error {
+    c, err := net.Dial("unix", socketFile)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	
+	connection = c
+	return nil
+}
+
+func WriteBuf(data []byte) error {
+	// encode data
+
+
+	// write data to buffer
+	_, err := connection.Write(data)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return nil
+}
+
+func ReadBuf() error {
+	// read data from buffer
+	buf := make([]byte, 32)
+	n, err := connection.Read(buf)
+	if err != nil {
+		return err
+	}
+
+	// decode data
+	anchor, value := beson.Deserialize(buf, 0)
+    fmt.Println("anchor =", anchor)
+    fmt.Println("value =", value.(*types.String).Get())
+	
+    println("Client got:", buf)
+	println("Client got:", string(buf[0:n]))
+
+	return nil
+}
